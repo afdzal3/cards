dd40c7cebb00006a01ffd96341c8fddfa4734eca
Afdzal <afdzal3@gmail.com>
2022-12-18 16:59:22 +0800

    placeholders

---
+++ app/Http/Controllers/CardController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class CardController extends Controller
+{
+    public function index(Request $req)
+    {
+
+        return "test";
+
+    }
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/card', [App\Http\Controllers\CardController::class,'index'])->name('card.index');
